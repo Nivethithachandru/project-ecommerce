718d9a102b0521894efcbc70316518d57cf333b4
Nivethithachandru <nivekrish43@gmail.com>
2025-08-04 17:54:43 +0530

    3

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 set -e
-set -x
 
-sudo cp -r demo /etc/nginx/sites-available/demo
+sudo cp -rf demo /etc/nginx/sites-available/demo
 
 if [ ! -L /etc/nginx/sites-enabled/demo ]; then
     sudo ln -s /etc/nginx/sites-available/demo /etc/nginx/sites-enabled/demo
