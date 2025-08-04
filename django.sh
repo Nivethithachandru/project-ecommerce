d3537d0a10756318c3388bdadea2901de0746971
Nivethithachandru <nivekrish43@gmail.com>
2025-08-04 17:12:45 +0530

    add ecommerce file

---
+++ django.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd /var/lib/jenkins/workspace
+
+source "venv/bin/activate"
+
+cd /var/lib/jenkins/workspace/project_Ecommerce
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+
+echo " Migrations and static file collection completed."
+
+if [ -f "ecommerce.service" ]; then
+    sudo cp -rf "ecommerce.service" /etc/systemd/system/
+    sudo systemctl daemon-reload
+    sudo systemctl enable "ecommerce.service"
+    sudo systemctl restart "ecommerce.service"
+    echo "Ecommerce service deployed and restarted."
+else
+    echo "ecommerce.service not found in $PROJECT_DIR"
+    exit 1
+fi
